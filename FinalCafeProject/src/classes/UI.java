d5369bee7fcb5fd52901dc7f67ae487bca403cfa
Romir Tandon <83617756+BottleOfBeans@users.noreply.github.com>
2022-05-31 12:28:07 -0400

    Redoing the Form For Final Cafe
    On God + Ratio

--- FinalCafeProject/src/classes/UI.java
+++ FinalCafeProject/src/classes/UI.java
@@ -3,14 +3,15 @@ package classes;
 import javax.swing.*;
 
 public class UI {
-    private JButton transferFundsButton;
-    private JButton displayAccountsButton;
-    private JButton processLunchButton;
-    private JButton closeAccountsButton;
-    private JButton valuedGiftButton;
-    private JTextField addInputHereTextField;
-    private JTextField enterYourInputHereTextField;
-    private JTextArea textArea1;
-
-
+    private JTextPane OutputArea;
+    private JButton closeAccountInputIDButton;
+    private JButton displayAccountsInputNullButton;
+    private JButton transferFundsInputRecpientButton;
+    private JButton processLunchInputIDButton;
+    private JButton valuedGiftInputNullButton;
+    private JTextField ProcessLunchInput;
+    private JTextField TransferFundsInput;
+    private JTextField DisplayAccountInput;
+    private JTextField CloseAccountInput;
+    private JTextField ValuedGiftInput;
 }
