f7ccd0a4a9e444c57204543c8d1e2230e2e38acb
Romir Tandon <83617756+BottleOfBeans@users.noreply.github.com>
2022-05-31 12:36:15 -0400

    Adding some more code :smile:
    Added some things to the cafeteria class, making sure that everything is actually finished before UI is updated further

--- FinalCafeProject/src/classes/cafeteria.java
+++ FinalCafeProject/src/classes/cafeteria.java
@@ -1,5 +1,16 @@
 package classes;
 
 public class cafeteria {
+    public cafeteria(){
+        ;
+    }
+    public void processLunch(){
+        ;
+    }
+    public void displayAccounts(){
+        manager.listAccounts();
+    }
+
+
 
 }
