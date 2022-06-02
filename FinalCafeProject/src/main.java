1d89926f4437f17fa58d7fb0f90b16b4d64f060d
Romir Tandon <83617756+BottleOfBeans@users.noreply.github.com>
2022-06-02 14:45:46 -0400

    Commenting out main
    Adding some comments to make it easier to grade.

--- FinalCafeProject/src/main.java
+++ FinalCafeProject/src/main.java
@@ -8,16 +8,18 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+//The main class that does everything
 public class main {
     public static void main(String[] args) throws Exception {
 
+        //UI setup code, making it visible and what to do when closed
         JFrame frame = new JFrame("Main");
         frame.setContentPane(new UI().mainPanel);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
 
-
+        //Creating 3 accounts for testing, with name value and ID and valued customer status
         lunchAccount Josh = new lunchAccount(100, 13482, true, "Josh");
         manager.addObject(Josh);
 
@@ -27,11 +29,12 @@ public class main {
         lunchAccount Luca = new lunchAccount(300, 14582, true, "Luca");
         manager.addObject(Luca);
 
+        //Checking if the manager class works and debugging code
         List<lunchAccount> accountList = manager.listAccounts();
         lunchAccount FirstAccount = accountList.get(0);
         System.out.println(FirstAccount.getID());
 
-        //MainMenu();
+        //Debugging display and lunch account code
         System.out.println(Josh.Display());
         System.out.println(Will.Display());
 
